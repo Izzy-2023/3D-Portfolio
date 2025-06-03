8094d70e727106df79eeede3389511d8d6479c9b
izzy-2023 <izzyzinxhirija@gmail.com>
2025-06-03 10:46:14 -0400

    added 3d model and navitation

--- src/components/RenderModel.jsx
+++ src/components/RenderModel.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+"use client"
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import clsx from 'clsx';
+import { Environment } from '@react-three/drei';
 
-const RenderModel = ({childern, className}) => {
+const RenderModel = ({children, className}) => {
   return (
     <Canvas>
-      className={clsx("w-screen h-screen relative", className)}
+      className={clsx("w-screen h-screen -z-10 relative", className)}
       <Suspense fallback={null}>
-        {childern}
+        {children}
       </Suspense>
+      <Environment preset='dawn'/>
     </Canvas>
   )
 }
